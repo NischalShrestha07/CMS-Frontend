7263087d56a1472ca72ff7309047918507363ff8
Nischal <nischalshrestha800@gmail.com>
2024-05-31 23:26:28 +0545

    React Manages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 
 import './App.css'
+// above line in NodeJS => require("./App.css") 
 
 function App() {
 
+
+  const myCssObject = {
+    fontFamily: "cursive",
+    fontSize: "25px"
+  }
   return (
     <>
-      <h1>Nischal will build a big house for his father and mother</h1>
+      {/* use of inner CSS by using css name like below */}
+      <h1 style={myCssObject}>Nischal Shrestha</h1>
+
+      {/* use of inline CSS  */}
+      <h3 style={{ color: "greenyellow", fontFamily: "cursive", background: "red", padding: "25px", borderRadius: "25px" }}>I am a Student</h3>
+      <h5>Age:{11 + 10}</h5>
 
     </>
   )
 }
 
 export default App
+// above line in NodeJS => 
+// module.exports=App
+// exports.App.css
