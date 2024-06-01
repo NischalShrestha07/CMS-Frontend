a51c0628b871f0ec03d7d1c133e06681f5848ed4
Nischal <nischalshrestha800@gmail.com>
2024-06-01 10:53:02 +0545

    Managing Different Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
-import './App.css'
-import { } from 'react'
+// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
 
 
   return (
-    <>
+    <BrowserRouter>
 
+      <Routes>
+        <Route path='/' element={<h1>I am in home page</h1>} />
+        <Route path='/about' element={<h1>i am from About page.</h1>} />
+        <Route path='/contact' element={<h1>i am from Contact page.</h1>} />
+      </Routes>
 
-
-    </>
+    </BrowserRouter>
   )
 }
 
