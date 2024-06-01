aede52fb21648a1936507ed31c1952d50f70f22a
Nischal <nischalshrestha800@gmail.com>
2024-06-01 10:21:06 +0545

    React Managing Setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 
 import './App.css'
-// above line in NodeJS => require("./App.css") 
-const name = "Komeyta"
+import { } from 'react'
 function App() {
 
-  // inner CSS by using CSS name mentioned below:
-  const myCssObject = {
-    fontFamily: "cursive",
-    fontSize: "25px"
-  }
+
   return (
     <>
-      {/* use of inner CSS by using css name like below */}
-      <h1 style={myCssObject}>Nischal Shrestha</h1>
 
-      {/* use of inline CSS  */}
-      <h3 style={{ color: "greenyellow", fontFamily: "cursive", background: "red", padding: "25px", borderRadius: "25px" }}>I am a Student</h3>
-      <h5>Age:{11 + 10}</h5>
-      <h1>{name}</h1>
 
 
     </>
@@ -25,6 +14,3 @@ function App() {
 }
 
 export default App
-// above line in NodeJS => 
-// module.exports=App
-// exports.App.css
