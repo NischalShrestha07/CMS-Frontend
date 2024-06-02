6a7b93f13586864dc59b3bba466d060b67153987
Nischal <nischalshrestha800@gmail.com>
2024-06-02 08:06:42 +0545

    React Tuts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,17 @@
 import React from 'react'
 
 const Home = () => {
+    const manage = {
+        fontSize: "45px",
+        background: "red",
+        color: "blue",
+        fontFamily: "cursive"
+
+    }
     return (
         <div>
-            This is rendered from Home Component
+            <h1>I am Nischal Shrestha</h1>
+            <span style={manage}>I am doing Bachelors in Computer Enginnerring.</span>
         </div>
     )
 }
