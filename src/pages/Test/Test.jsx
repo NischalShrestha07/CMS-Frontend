4e14a5ad02ac85236b6cff14417f43cdc7144279
Nischal <nischalshrestha800@gmail.com>
2024-06-04 11:58:45 +0545

    React Frontend

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,13 +1,47 @@
-
 import { useState } from "react"
 
+
 const Test = () => {
-    const [chauchau, setChauchau] = useState(0)
+    const [count, setCount] = useState(0)
+
+    //normal Function
+    // function increaseCount() {
+    //     setCount(count + 1)
+    //     console.log(count);
+    // }
+    // function decreaseCount() {
+    //     setCount(count - 1)
+    //     console.log(count);
+    // }
+
+    // ARROW FUNCTION:
+    const increaseCount = () => {
+        setCount(count + 1)
+        console.log(count);
+
+    }
+
+    const decreaseCount = () => {
+        if (count > 0) {
+
+            setCount(count - 1)
+            console.log(count);
+        }
+        else {
+            stop
+        }
+    }
+
+
+
 
     return (
         <div>
-            {chauchau}
+            <h2>{count}</h2>
+            <button onClick={increaseCount}>+</button>
+            <button onClick={decreaseCount}>-</button>
         </div>
+
     )
 }
 
