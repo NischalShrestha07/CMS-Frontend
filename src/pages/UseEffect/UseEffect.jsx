f834be30474968945752bc9921e427190f60fb4e
Nischal <nischalshrestha800@gmail.com>
2024-06-05 21:11:09 +0545

    React Frontend

--- src/pages/UseEffect/UseEffect.jsx
+++ src/pages/UseEffect/UseEffect.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 const UseEffect = () => {
-    const [count, setCount] = useState(0)
+    const [increaseCount, setIncreaseCount] = useState(0)
     const [decreaseCount, setDecreaseCount] = useState(0)
-    const increaseCount = () => {
-        setCount(count + 1)
+
+    //function to increase the count value. 
+    const handleIncrease = () => {
+        setIncreaseCount(increaseCount + 1)
         // console.log(count);
     }
 
@@ -19,16 +21,16 @@ const UseEffect = () => {
     // Type1 use of useEffect(jaba certain value eg: like numbers etc haru increase garera backend ma store garne. )
     useEffect(() => {
         // count ko value kati xa tyo backend samma pugauxa yesle:
-        if (count !== 0) {
+        if (increaseCount !== 0) {
             // document.title = count
             console.log("Count value Changed.");
         }
-    }, [count, decreaseCount])
+    }, [increaseCount, decreaseCount])
 
     return (
         <div>
-            <div >{count}</div>
-            <button onClick={increaseCount}>+</button>
+            <div >{increaseCount}</div>
+            <button onClick={handleIncrease}>+</button>
             <div>{decreaseCount}</div>
             <button onClick={handleDecrease}>-</button>
         </div>
